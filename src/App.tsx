f20af90c461480b445015743aa9289d449b26654
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2022-06-30 01:40:28 +0530

    first and final commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+import NewReminder from "./components/NewReminder"
+import ReminderList from "./components/ReminderList"
+import Reminder from "./models/reminder"
+import reminderService from "./services/reminder"
+
+function App() {
+	const [reminders, setReminders] = useState<Reminder[]>([])
+
+	useEffect(() => {
+		loadReminders()
+	}, [])
+
+	const loadReminders = async () => {
+		const reminders = await reminderService.getReminders()
+		setReminders(reminders)
+	}
+
+	const removeReminder = (id: number) => {
+		setReminders(reminders.filter((reminder) => reminder.id !== id))
+	}
+
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title)
+    setReminders([newReminder, ...reminders])
+  }
+
+	return (
+		<div className="App">
+      <NewReminder onAddReminder={addReminder} />
+			<ReminderList items={reminders} onRemoveReminder={removeReminder} />
+		</div>
+	)
+}
+
+export default App
